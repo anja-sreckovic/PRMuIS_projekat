7d6ef59a35073d17eae072323c6b0365ec5eec7f
Milan Stojanović <stojanovic.pr15.2023@uns.ac.rs>
2026-01-31 17:09:59 +0100

    Initial commit

---
+++ Server/Server/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Server
+{
+    public partial class MainWindow : Window
+    {
+        private Socket _listener;
+        private readonly List<Socket> _clients = new List<Socket>();
+        private readonly object _lock = new object();
+
+        private CancellationTokenSource _cts;
+        private Task _serverTask;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StopServer();
+        }
+
+        private void StopServer()
+        {
+            
+        }
+
+        private void BroadcastBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ServerLoop(CancellationToken token)
+        {
+            
+        }
+
+        private void AcceptClient(Socket listener)
+        {
+            
+        }
+
+        private void ReceiveFromClient(Socket client)
+        {
+            
+        }
+
+        private void Broadcast(string message)
+        {
+            
+        }
+
+        private void SendToClient(Socket client, string msg)
+        {
+            
+        }
+
+        private void RemoveClient(Socket client, string reason)
+        {
+            
+        }
+
+        private static void SafeClose(Socket s)
+        {
+            
+        }
+
+        private void Log(string line)
+        {
+            
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            StopServer();
+            base.OnClosed(e);
+        }
+
+        private void BroadcastBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
